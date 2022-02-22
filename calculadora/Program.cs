6c06936bf26041189b43353a6d9f7e7ebfe85a7c
aluno <aluno@etehort.ps.sp>
2022-02-22 20:51:18 -0300

    exercicio1calculadora

---
+++ calculadora/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("calculadora insira o primeiro número");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("calculadora /insira o primeiro número");
+            double b = double.Parse(Console.ReadLine());
+            double soma = a + b;
+            double sub = a - b;
+            double mult = a * b;
+            double div = a / b;
+            Console.WriteLine(soma.ToString() +" "+ sub.ToString() +" "+ mult.ToString() +" "+ div.ToString());
+
+        }
+    }
+}
